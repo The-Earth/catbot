552376a86b300b7f7e76dea8b18fad5276311b19
The-Earth <tigerzeng97@gmail.com>
2020-10-13 20:34:57 +0800

    [Add](setup.py)

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='catbot',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/The-Earth/catbot',
+    license='MIT License',
+    author='The-Earth',
+    author_email='',
+    description='',
+    install_requires='requests'
+)
