71e646647fda978ffa5be4561beffc2c89a4740b
The-Earth <tigerzeng97@gmail.com>
2020-10-17 12:01:37 +0800

    [Modify](setup) update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='catbot',
-    version='0.1',
+    version='0.2',
     packages=[''],
     url='https://github.com/The-Earth/catbot',
     license='MIT License',
