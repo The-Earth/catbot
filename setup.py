53147bacfdeb53148ee46ace13d8f7b8025ab9f5
The-Earth <26201356+The-Earth@users.noreply.github.com>
2021-10-16 13:02:20 -0400

    [Fix](catbot) Fix index error in 0.6.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='catbot',
-    version='0.6.2',
+    version='0.6.3',
     packages=[''],
     url='https://github.com/The-Earth/catbot',
     license='MIT License',
